5cd60912014746eb64c96bb805ce9c9bb4be4aa0
littlewhelan <klwhelan@gmail.com>
2015-10-14 11:40:38 -0500

    added angular repeats in search html

--- routes/search.js
+++ routes/search.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
 
     var searchComp = 'SELECT contactFirstName AS CFN, contactLastName AS CLN, name AS COMPANY, contactPhone AS CP, contactEmail AS CE FROM corpDonors WHERE name REGEXP "' + searchString + '" UNION SELECT contactFirstName AS CFN, contactLastName AS CLN, name AS COMPANY, contactPhone AS CP, contactEmail AS CE FROM corpDonors WHERE contactFirstName REGEXP "' + searchString + '" UNION SELECT name AS COMPANY, contactFirstName AS CFN, contactLastName AS CLN, contactPhone AS CP, contactEmail AS CE FROM corpDonors WHERE contactLastName REGEXP "' + searchString + '" ';
 
-    var searchChild ='SELECT adultOneFirstName AS A1FN, adultOneLastName AS A1LN, emerPhone AS EP, firstName AS FN, lastName AS LN FROM families f, children c WHERE f.id = c.familyID AND lastName REGEXP "' + searchString + '" UNION SELECT adultOneFirstName AS A1FN, adultOneLastName AS A1LN, emerPhone AS EP, firstName AS FN, lastName AS LN FROM families f, children c WHERE f.id = c.familyID AND firstName REGEXP "' + searchString + '" ';
+    var searchChild ='SELECT adultOneFirstName AS CA1FN, adultOneLastName AS CA1LN, mainPhone AS CMP, firstName AS CFN, lastName AS CLN FROM families f, children c WHERE f.id = c.familyID AND lastName REGEXP "' + searchString + '" UNION SELECT adultOneFirstName AS CA1FN, adultOneLastName AS CA1LN, mainPhone AS CMP, firstName AS CFN, lastName AS CLN FROM families f, children c WHERE f.id = c.familyID AND firstName REGEXP "' + searchString + '" ';
 
 
     con.connect(function(err) {
@@ -42,18 +42,14 @@ router.get('/', function(req, res, next) {
             con.query(searchA1, function (err, rows, fields) {
                 if (err) throw err;
                    rows.forEach(checkArray);
-                console.log('rowsA1', rows);
 
                 con.query(searchA2, function (err, rows, fields) {
                     if (err) throw err;
                     rows.forEach(checkArray);
-                    console.log('rowsA2', rows);
-
 
                     con.query(searchComp, function (err, rows, fields) {
                         if (err) throw err;
                         rows.forEach(checkArray);
-                        console.log('rowsComp', rows);
 
                         con.query(searchChild, function (err, rows, fields) {
                             if (err) throw err;
