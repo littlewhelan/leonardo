6ba585e08ed4a3ecf6bb7b92be6c2e5f7f24916d
littlewhelan <klwhelan@gmail.com>
2015-10-17 10:52:18 -0500

    seperated out file structure

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -46,76 +46,8 @@ app.config(['$routeProvider',
 
 
 
-//edit corporation modal template
-app.controller('editCorpCtrl', ['$scope', '$uibModal', '$log', 'ResultService', function ($scope, $uibModal, $log, ResultService) {
-    //sends the companies after the results have been organized
-    $scope.companies = ResultService.companies;
-
-    $scope.animationsEnabled = true;
-
-    $scope.openCorp = function (size) {
-
-        var modalInstance = $uibModal.open({
-            animation: $scope.animationsEnabled,
-            templateUrl: 'private/editcorp.html',
-            controller: 'ModalInstanceCtrl',
-            size: size,
-            resolve: {
-                items: function () {
-                    return $scope.items;
-                }
-            }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
-    };
-
-    $scope.toggleAnimation = function () {
-        $scope.animationsEnabled = !$scope.animationsEnabled;
-    };
-
-}]);
-
-//edit family modal template
-app.controller('editFamilyCtrl',['$scope', '$uibModal', '$log','ResultService', function ($scope, $uibModal, $log, ResultService) {
-    //sends the adults after the results have been organized
-    $scope.adults = ResultService.adults;
-    //sends the kids after the results have been organized
-    $scope.kids = ResultService.kids;
 
 
-    $scope.animationsEnabled = true;
-
-    $scope.open = function (size) {
-
-        var modalInstance = $uibModal.open({
-            animation: $scope.animationsEnabled,
-            templateUrl: 'private/editfamily.html',
-            controller: 'ModalInstanceCtrl',
-            size: size,
-            resolve: {
-                items: function () {
-                    return $scope.items;
-                }
-            }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
-    };
-
-    $scope.toggleAnimation = function () {
-        $scope.animationsEnabled = !$scope.animationsEnabled;
-    };
-
-}]);
 
 // Please note that $modalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
