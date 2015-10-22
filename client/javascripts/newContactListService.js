6af165fab785d40ccdf3a4bae0184a4ee9c0b320
Kamiethenerd <Kamiethenerd@gmail.com>
2015-10-22 11:04:16 -0500

    10/22.2

--- client/javascripts/newContactListService.js
+++ client/javascripts/newContactListService.js
@@ -1,6 +1,6 @@
 //service for ajax calls to constant contact
 
-app.service('contactListData', ['contactListDataService','$scope','$http', function(contactListDataService, $scope, $http) {
+app.service('newContactListData', ['contactListDataService','$scope','$http', function(contactListDataService, $scope, $http) {
 
     var newContactList = [];
     var listNum = "1499523610";
