f86a3a8650dc63c59c1a3380d5eab9a8f6566eb1
TShaunik <shawnf1@gmail.com>
2015-10-23 09:35:58 -0500

    Merging files from katie

--- client/javascripts/createListContoller.js
+++ client/javascripts/createListContoller.js
@@ -1,25 +1,75 @@
-app.controller('createListSearch',['$scope', '$rootScope', 'ContactListDataService', function($scope, $rootScope, ContactListDataService){
-    $rootScope.add = false;
-    $scope.listForm={};
-    $scope.searchListBtn = function () {
-		//console.log("list type", $scope.listType);
-		var $type = $('#listType :checked');
-		// check if it's age or zip, requiring additional input
-		if($type.hasClass('js-extra')) {
-			console.log("needs extra info");
-			var value = $type.next().next().val();
-		}
-		var id = $type.attr('id');
-		//var $types = $('#listType');
-		//if($types.find('input:checked').hasClass('js-extra')) {
-		//	var value = $types.find('input:checked').hasClass('js-extra').siblings('.js-value').val();
-		//}
-		//var type = $types.find('input:checked').hasClass('js-extra').attr('id');
-		console.log("type ", id, " value ", value);
-		//console.log("value", value);
-        //passes in search text to service
-        //ContactListDataService.makeDataCall($scope.listForm.data);
-        //empty the search box
-        $scope.listForm={};
-    };
+//app.controller('createListSearch',['$scope', '$rootScope', 'ContactListDataService', function($scope, $rootScope, ContactListDataService){
+//    $rootScope.add = false;
+//    $scope.listForm={};
+//    $scope.searchListBtn = function () {
+//		//console.log("list type", $scope.listType);
+//		var $type = $('#listType :checked');
+//		// check if it's age or zip, requiring additional input
+//		if($type.hasClass('js-extra')) {
+//			console.log("needs extra info");
+//			var value = $type.next().next().val();
+//		}
+//		var id = $type.attr('id');
+//		//var $types = $('#listType');
+//		//if($types.find('input:checked').hasClass('js-extra')) {
+//		//	var value = $types.find('input:checked').hasClass('js-extra').siblings('.js-value').val();
+//		//}
+//		//var type = $types.find('input:checked').hasClass('js-extra').attr('id');
+//		console.log("type ", id, " value ", value);
+//		//console.log("value", value);
+//        //passes in search text to service
+//        //ContactListDataService.makeDataCall($scope.listForm.data);
+//        //empty the search box
+//        $scope.listForm={};
+//    };
+//}]);
+app.controller('createListSearch',['$scope','$http', 'ContactListDataService', function($scope, $http, ContactListDataService) {
+	$scope.corp ={};
+	$scope.fam ={};
+	$scope.zipCk ={};
+	$scope.ageCk ={};
+	$scope.age ={};
+	$scope.zip={};
+
+	$scope.searchListBtn = function () {
+		//passes in search text to service
+		console.log('this is the radios' , $scope.corp,$scope.fam,$scope.zip,$scope.age, $scope.zipCk, $scope.ageCk);
+		console.log($scope.zip.data);
+
+
+
+		//search by zip if zipCK is true
+
+		var promise = ContactListDataService.makeDataCall($scope.zip.data);
+
+		promise.then(function (results) {
+			$scope.emailList = results.data;
+		});
+
+		////search by age if ageCk is true
+		//var promise = ContactListDataService.makeDataCall($scope.age.data);
+		//promise.then(function (results) {
+		//	$scope.emailList = results.data;
+		//});
+
+		////get all families if fam is true
+		//var promise = ContactListDataService.makeDataCall();
+		//promise.then(function (results) {
+		//	$scope.emailList = results.data;
+		//	console.log(results.data);
+		//});
+		//
+		////get all corp if corp is true
+		//ContactListDataService.makeDataCall();
+		//
+
+		//empty the search box
+		$scope.corp ={};
+		$scope.fam ={};
+		$scope.zipCk ={};
+		$scope.ageCk ={};
+		$scope.age ={};
+		$scope.zip={};
+	};
+
 }]);
--- routes/createMailList.js
+++ routes/createMailList.js
@@ -1,3 +1,88 @@
+//var express = require('express');
+//var router = express.Router();
+//var mysql = require('mysql');
+//var db = require('../config/db.js');
+//var ser = require('../models/createListQ');
+//
+//
+///*GET for DB search*/
+//router.get('/', function(req, res, next) {
+//
+//	var con = mysql.createConnection(db);
+//
+//
+////making the connection to the database
+//
+//	con.connect(function (err) {
+//		if (err) {
+//			console.log('Error connecting to Db');
+//			return;
+//		}
+//		console.log('Connection established');
+//
+////choosing which situation is true for creating the mailing list
+////donors is selected from the list
+//
+//		switch (expression) {
+//			case families:
+//
+//				con.query(familyList, function (err, rows) {
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				})
+//				break;
+//
+//
+//			case corporation:
+//				con.query(donorList, function (err, rows) {
+//					console.log('You are in the query!');
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				});
+//
+//			case zip:
+//				con.query(zipList, [searchString], function (err, rows) {
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				});
+//				break;
+//
+//			case age:
+//				con.query(ageList, [searchString], function (err, rows) {
+//					console.log('You are in the query!');
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				});
+//				break;
+//
+//			default:
+//				res.send('Please Select an option');
+//		}
+//
+//	});
+//
+//});
+//
+//
+//module.exports = router;
+
+
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
@@ -6,9 +91,12 @@ var ser = require('../models/createListQ');
 
 
 /*GET for DB search*/
-router.get('/', function(req, res, next) {
+router.get('/*', function(req, res, next) {
 
 	var con = mysql.createConnection(db);
+	console.log("received from radio buttons", req.query.search);
+	var searchString = req.query.search;
+	console.log("received from radio buttons", searchString);
 
 
 //making the connection to the database
@@ -20,64 +108,91 @@ router.get('/', function(req, res, next) {
 		}
 		console.log('Connection established');
 
+
+		con.query(ser.zipList, [searchString,searchString,searchString], function (err, rows) {
+			if (err) throw err;
+			console.log('Data received from Db:\n');
+			console.log(rows);
+			con.end();
+			res.send(rows);
+
+		});
+
+
+		//con.query(ser.ageList, [searchString], function (err, rows) {
+		//	console.log('You are in the query!');
+		//
+		//	if (err) throw err;
+		//	console.log('Data received from Db:\n');
+		//	console.log(rows);
+		//	con.end();
+		//	res.send(rows);
+		//
+		//});
+
+	});
+});
+
+module.exports 	= router;
+
+
+
+
+
+
 //choosing which situation is true for creating the mailing list
 //donors is selected from the list
 
-		switch (expression) {
-			case families:
+//		switch (expression) {
+//			case families:
+//
+//				con.query(familyList, function (err, rows) {
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				});
+//				break;
+//
+//
+//			case corporation:
+//				con.query(donorList, function (err, rows) {
+//					console.log('You are in the query!');
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				});
+//
+//			case zip:
+//				con.query(zipList, [searchString], function (err, rows) {
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				});
+//				break;
+//
+//			case age:
+//				con.query(ageList, [searchString], function (err, rows) {
+//					console.log('You are in the query!');
+//
+//					if (err) throw err;
+//					console.log('Data received from Db:\n');
+//					console.log(rows);
+//					con.end();
+//					res.send(rows);
+//				});
+//				break;
+//
+//			default:
+//				res.send('Please Select an option');
+//		}
 
-				con.query(familyList, function (err, rows) {
-
-					if (err) throw err;
-					console.log('Data received from Db:\n');
-					console.log(rows);
-					con.end();
-					res.send(rows);
-				})
-				break;
-
-
-			case corporation:
-				con.query(donorList, function (err, rows) {
-					console.log('You are in the query!');
-
-					if (err) throw err;
-					console.log('Data received from Db:\n');
-					console.log(rows);
-					con.end();
-					res.send(rows);
-				});
-
-			case zip:
-				con.query(zipList, [searchString], function (err, rows) {
-
-					if (err) throw err;
-					console.log('Data received from Db:\n');
-					console.log(rows);
-					con.end();
-					res.send(rows);
-				});
-				break;
-
-			case age:
-				con.query(ageList, [searchString], function (err, rows) {
-					console.log('You are in the query!');
-
-					if (err) throw err;
-					console.log('Data received from Db:\n');
-					console.log(rows);
-					con.end();
-					res.send(rows);
-				});
-				break;
-
-			default:
-				res.send('Please Select an option');
-		}
-
-	});
-
-});
-
-
-module.exports = router;
+//
