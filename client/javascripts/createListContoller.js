bfafe93b5ee6c3d614d8d622782d1f1f704d3dbc
littlewhelan <klwhelan@gmail.com>
2015-10-23 14:22:27 -0500

    id is set

--- client/javascripts/createListContoller.js
+++ client/javascripts/createListContoller.js
@@ -19,6 +19,7 @@ app.controller('createListSearch', ['$scope', '$http', 'ContactListDataService',
 
         //THIS WORKS  -- will search by zip
         if ($scope.zipCk == true) {
+
             ContactListDataService.makeDataCall('zip', $scope.zip.data)
                 .then(function (data) {
                     $scope.emailList = data;
@@ -30,27 +31,27 @@ app.controller('createListSearch', ['$scope', '$http', 'ContactListDataService',
         //THIS WORKS - will search by age
         if ($scope.ageCk == true) {
             var age = ContactListDataService.makeDataCall('age', $scope.age.data);
-            age.then(function (results) {
-                console.log(results.data);
-                $scope.emailList = results.data;
+            age.then(function (data) {
+                console.log(data);
+                $scope.emailList = data;
             });
         }
 
         //THIS WORKS --get all families if fam is true
         if ($scope.fam == true) {
             var fam = ContactListDataService.makeDataCall('family', '');
-            fam.then(function (results) {
-                $scope.emailList = results.data;
-                console.log(results.data);
+            fam.then(function (data) {
+                $scope.emailList = data;
+                console.log(data);
             });
         }
 
         //get all corp if corp is true
         if ($scope.corp == true) {
             var comp = ContactListDataService.makeDataCall('company', '');
-            comp.then(function (results) {
-                $scope.emailList = results.data;
-                console.log(results.data);
+            comp.then(function (data) {
+                $scope.emailList = data;
+                console.log(data);
             });
         }
 
