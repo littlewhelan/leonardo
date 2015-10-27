7f30e66bb9444f4e0edfc3812e3318795812a6ca
littlewhelan <klwhelan@gmail.com>
2015-10-27 09:58:50 -0500

    constant contact works and emer contat madal style fixed

--- client/javascripts/newContactListService.js
+++ client/javascripts/newContactListService.js
@@ -1,39 +1,15 @@
 
 app.service('newContactListData', ['ContactListDataService', '$http',
     function (ContactListDataService, $http) {
-        this.newContactList = [];
-        this.listNum = "";
+        var listNum = "";
         var importDataArray =[];
-        //this.importDataArray = [
-        //    {
-        //        "email_addresses": [
-        //            "user1@example.com"
-        //        ],
-        //        "first_name": "John",
-        //        "last_name": "Smith",
-        //        "company_name": "Company X",
-        //        "addresses": [{
-        //            city: "anytown"
-        //        }]
-        //    },
-        //    {
-        //        "email_addresses": [
-        //            "user2@example.com"
-        //        ],
-        //        "first_name": "Jane",
-        //        "last_name": "Smithy",
-        //        "company_name": "Company Y",
-        //        "addresses": [{
-        //            city: "sometown"
-        //        }]
-        //    }
-        //];
+
         this.postList = function (name) {
             console.log("did the name make it to the service? " + name );
             return $http.post('/newContactList', {name: name}).then(function (response) {
                 console.log(response.data);
-                this.listNum = response.data;
-                console.log("Id? ", this.listNum);
+                listNum = response.data;
+                console.log("Id? ", listNum);
             }, function(data){
                 console.log(data); //error
             });
@@ -41,7 +17,7 @@ app.service('newContactListData', ['ContactListDataService', '$http',
         this.popList = function (list) {
             this.dataObject = {
                 "importDataArray": importDataArray,
-                "listNum": this.listNum
+                "listNum": listNum
             };
             console.log('got to popList');
             console.log('importDataArray: ',this.importDataArray, '\ntype: ', typeof this.importDataArray);
