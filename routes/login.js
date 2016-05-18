15f531a406332c5cbd32dc0f59928c8da22e4d46
TShaunik <shawnf1@gmail.com>
2016-05-18 17:52:21 -0500

    Auth won't work after validator. Try before.

--- routes/login.js
+++ routes/login.js
@@ -16,6 +16,15 @@ router.get('/', function (req, res, next) {
 /* POST /api/register/ */
 router.post('/', function (req, res, next) {
 	var login = req.body;
+	User.getAuthenticated(req.body, function (err, token) {
+		if (err) {
+			console.log("routes",err.message);
+			res.status(400).send(err.message);
+		} else {
+			console.log("token", token);
+			res.send(token);
+		}
+	});
 	console.log("In login post route", login, loginCheck);
 	validator.run(loginCheck, login, function (errCount, err) {
 		console.log("after validator check", errCount, err);
