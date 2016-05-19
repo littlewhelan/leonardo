1b6a02f1904f09bec3506e38f68b0d32aa17871a
TShaunik <shawnf1@gmail.com>
2016-05-18 20:54:21 -0500

    Testing if node-validator now only runs function if there's an error.

--- routes/login.js
+++ routes/login.js
@@ -28,36 +28,39 @@ router.post('/', function (req, res, next) {
 	// 		res.send(token);
 	// 	}
 	// });
-	console.log("In login post route", login, loginCheck);
+	console.log("In login post route", login);
 	validator.run(loginCheck, login, function (errCount, err) {
 		// console.log("after validator check", errCount, err);
 		if(errCount > 0) {
 			console.log("validator found errors in request", errCount, err);
 			return res.sendStatus(400);
-		}else {
-			console.log("No errors found in login request");
 		}
 		console.log("passed validator check for login");
+		// User.getAuthenticated(req.body, function (err, token) {
+		// 	if (err) {
+		// 		console.log("routes",err.message);
+		// 		res.status(400).send(err.message);
+		// 	} else {
+		// 		console.log("token", token);
+		// 		res.send(token);
+		// 	}
+		// });
+	});
+	console.log("after validator section");
+	if(!res.headerSent) {
+		console.log("headers haven't been sent");
 		User.getAuthenticated(req.body, function (err, token) {
 			if (err) {
 				console.log("routes",err.message);
-				res.status(400).send(err.message);
+				return res.status(400).send(err.message);
 			} else {
 				console.log("token", token);
-				res.send(token);
+				return res.send(token);
 			}
 		});
-	});
-	console.log("after validator section, fail");
-	User.getAuthenticated(req.body, function (err, token) {
-		if (err) {
-			console.log("routes",err.message);
-			res.status(400).send(err.message);
-		} else {
-			console.log("token", token);
-			res.send(token);
-		}
-	});
+	}else {
+		console.log("headers have been sent");
+	}
 });
 
 
